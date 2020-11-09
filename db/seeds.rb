e68eee381485502667325b73b8fe8f2db0684d1a
Curtis Bartell <cbartell980@gmail.com>
2020-11-09 10:25:47 -0700

    Add doctor and hospital seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Hospitals:
+hospital1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
+hospital2 = Hospital.create!(name: 'Seaside Health & Wellness Center')
+
+# Doctors:
+doctor1 = hospital1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard University')
+doctor2 = hospital1.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins University')
+doctor3 = hospital1.doctors.create!(name: 'Miranda Bailey', specialty: 'General Surgery', university: 'Stanford University')
+doctor4 = hospital1.doctors.create!(name: 'Derek McDreamy Shepherd', specialty: 'Attending Surgeon', university: 'University of Pennsylvania')
